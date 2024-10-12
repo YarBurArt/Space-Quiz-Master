347e5716c5eb08be736af560b6e443bec1cb1183
Yaroslav <yaroslav.chayk@gmail.com>
2024-10-12 16:55:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ questions = {
         "explanation": "here's an explanation\n https://science.nasa.gov/mission/mer-opportunity/"
     },
     "hubble_telescope_info": {
-        "image": "",
+        "image": "https://science.nasa.gov/wp-content/uploads/2023/07/caldwell-69.jpg?w=2048&format=webp",
         "question": "What is the Hubble Space Telescope and what are its key discoveries?",
         "explanation": "The Hubble Space Telescope is a large, space-based observatory that"
             "has been in operation since 1990. It has made numerous groundbreaking discoveries, "
@@ -20,7 +20,7 @@ questions = {
     },
 
     "international_space_station_info": {
-        "image": "",
+        "image": "https://www.nasa.gov/wp-content/uploads/2021/08/44911459904-375bc02163-k-0.jpg?resize=768,512",
         "question": "What is the International Space Station and what kind of research is conducted there?",
         "explanation": "The International Space Station is a large, orbiting laboratory where"
             " astronauts from various countries conduct scientific experiments and research in fields"
@@ -30,7 +30,7 @@ questions = {
     },
 
     "james_webb_telescope_info": {
-        "image": "",
+        "image": "https://www.nasa.gov/wp-content/uploads/2023/02/International-Space-Station-in-2021.jpg?resize=1536,85",
         "question": "What is the James Webb Space Telescope and what are its key capabilities?",
         "explanation": "The James Webb Space Telescope is a large, infrared-optimized space observatory "
             "that was launched in 2021. It is designed to study the earliest galaxies in the universe,"
@@ -39,7 +39,8 @@ questions = {
     },
 
     "artemis_program_info": {
-        "image": "",
+        "image": "https://images-assets.nasa.gov/image/NHQ202211160002/NHQ202211160002~large.jpg"
+            "?w=1920&h=1193&fit=clip&crop=faces%2Cfocalpoint",
         "question": "What is the Artemis program and what are its goals?",
         "explanation": "The Artemis program is NASA's initiative to return humans to the Moon, "
             "with the ultimate goal of establishing a sustainable presence on the lunar surface."
@@ -48,7 +49,7 @@ questions = {
     },
 
     "perseverance_rover_info": {
-        "image": "",
+        "image": "https://science.nasa.gov/wp-content/uploads/2024/07/pia26344-2500x.jpg?w=2048&format=webp",
         "question": "What is the Perseverance rover and what is its mission on Mars?",
         "explanation": "The Perseverance rover is the latest Mars rover mission by NASA,"
             " launched in 2020. Its primary goals are to search for signs of ancient microbial life, "
@@ -57,7 +58,7 @@ questions = {
     },
 
     "parker_solar_probe_info": {
-        "image": "",
+        "image": "https://science.nasa.gov/wp-content/uploads/2023/09/Parker_Solar_Probe_wisp-800x600-2.jpg?w=2048&format=webp",
         "question": "What is the Parker Solar Probe and what is its purpose?",
         "explanation": "The Parker Solar Probe is a NASA spacecraft launched in 2018 with the goal"
             " of studying the Sun and its corona, the outermost part of the Sun's atmosphere."
@@ -66,7 +67,7 @@ questions = {
     },
 
     "james_webb_telescope_launch_info": {
-        "image": "",
+        "image": "https://live.staticflickr.com/65535/53389652523_77867aaf22_z.jpg",
         "question": "When was the James Webb Space Telescope launched and what was the launch process like?",
         "explanation": "The James Webb Space Telescope was launched on December 25, 2021, aboard "
         "an Ariane 5 rocket from the Guiana Space Center in French Guiana. The launch process was complex,"
